0ab603785e0ff40636d9fa1da7666a3cb35b613e
h <mlgdashyt@gmail.com>
2024-12-10 15:26:14 -0500

    ball movement doesnt have enough Y velocity, so for the "_velocity = new Vector2(Speed, 0);" code, I changed it to" _velocity = new Vector2(Speed, 200);", and decreased the speed from 400f to 300f

--- scenes/Ball.cs
+++ scenes/Ball.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class Ball : RigidBody2D
 {
-    [Export] public float Speed = 400f; // export variable for ball speed
+    [Export] public float Speed = 300f; // export variable for ball speed
 
     private Vector2 _velocity = Vector2.Zero; // variable to store ball's velocity
 
@@ -51,6 +51,6 @@ public partial class Ball : RigidBody2D
         Position = ballStartNode.Position;
 
         // initialize the ball's velocity moving to the right with the specified speed
-        _velocity = new Vector2(Speed, 0);
+        _velocity = new Vector2(Speed, 200);
     }
 }
