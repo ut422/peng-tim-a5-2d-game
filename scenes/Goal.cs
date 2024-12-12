e34063e702cedad4ac0c7479a185383252991b95
h <mlgdashyt@gmail.com>
2024-12-12 01:50:48 -0500

    polish

--- scenes/Goal.cs
+++ scenes/Goal.cs
@@ -3,14 +3,14 @@ using System;
 
 public partial class Goal : Area2D
 {
-    // Called when the area is ready to use
+    // called when the area is ready to use
     public override void _Ready()
     {
-        // Connect the "body_entered" signal of the Area2D to the function in this script
+        // connect the "body_entered" signal of the Area2D to the function in this script
         BodyEntered += _on_Goal_body_entered;
     }
 
-    // Called when a body enters the goal area
+    // called when a body enters the goal area
     private void _on_Goal_body_entered(Node body)
     {
         // check if the body entering the area is the ball
@@ -20,7 +20,7 @@ public partial class Goal : Area2D
             GD.PrintErr("Ball touched the goal. Quitting the game!");
 
             // quit the game
-            GetTree().Quit();  // This will close the game immediately
+            GetTree().Quit();  // closes the game immediately
         }
     }
 }
